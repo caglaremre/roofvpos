abee04dcfdce972056d05695e9c696fc817c1832
Emre Çağlar <caglar.emre01@gmail.com>
2026-01-21 21:20:15 +0300

    void template added void called from transaction list and js logic added.

--- assets/custom.js
+++ assets/custom.js
@@ -8,15 +8,17 @@ async function getIPAddress() {
 		const response = await fetch('https://ifconfig.me/ip');
 		const data = await response.text();
 		console.log("Your IP Address is:", data);
-		document.getElementById('sale-cardholder-ip').value =data
-		document.getElementById('sale-merchant-ip').value=data
-		document.getElementById('sale-submerchant-ip').value=data
+		document.getElementById('sale-cardholder-ip').value = data
+		document.getElementById('sale-merchant-ip').value = data
+		document.getElementById('sale-submerchant-ip').value = data
 
 	} catch (error) {
 		console.error("Error fetching IP:", error);
 	}
 }
+
 getIPAddress();
+
 async function updateConfig() {
 	const clientToken = document.getElementById('clientToken').value
 	const secretKey = document.getElementById('secretKey').value
@@ -37,3 +39,16 @@ async function updateConfig() {
 	}
 
 }
+
+document.getElementById('transaction-list').addEventListener('submit', async function (e) {
+	if (e.target.name === 'order-void') {
+		e.preventDefault();
+		const form = e.target
+		const orderID = form.querySelector('input[name="orderID"]').value;
+		const processID = form.querySelector('input[name="processID"]').value;
+		console.log("passing values to void canvas:", orderID, "Process:", processID);
+		document.getElementById('void-order-id').value = orderID;
+		document.getElementById('void-process-id').value = processID;
+		document.getElementById('void-offcanvas-button').click()
+	}
+})
