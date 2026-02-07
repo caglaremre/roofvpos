e16a0a6f303e896a884db00da2078b6be3244979
Emre Çağlar <caglar.emre01@gmail.com>
2026-02-07 16:37:32 +0300

    embed added for builded application

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"embed"
+	"html/template"
+	"io/fs"
 	"log"
+	"net/http"
 	"roof/vpos/repository"
 	"roof/vpos/routes"
 
 	"github.com/gin-gonic/gin"
 )
+
+//go:embed templates/*
+var templateFS embed.FS
+
+//go:embed assets/*
+var staticFS embed.FS
 
 func main() {
 
@@ -22,11 +32,26 @@ func main() {
 
 	//init gin
 	router := gin.Default()
+	tmpl := template.Must(template.ParseFS(templateFS, "templates/*"))
+	router.SetHTMLTemplate(tmpl)
 
-	router.StaticFile("/favicon.ico", "./assets/favicon.ico")
-	router.Static("/assets", "./assets")
-	router.LoadHTMLGlob("templates/*")
-
+	if gin.Mode() == "release" {
+		staticFS, _ := fs.Sub(staticFS, "assets")
+		router.StaticFS("/assets", http.FS(staticFS))
+		router.GET("/favicon.ico", func(c *gin.Context) {
+			fileData, err := fs.ReadFile(staticFS, "favicon.ico")
+			if err != nil {
+				// If this triggers, your file isn't where you think it is
+				c.String(404, "Favicon not found")
+				return
+			}
+			c.Data(200, "image/x-icon", fileData)
+		})
+	} else {
+		router.StaticFile("/favicon.ico", "./assets/favicon.ico")
+		router.Static("/assets", "./assets")
+		router.LoadHTMLGlob("templates/*")
+	}
 	routes.RegisterRoutes(bolt, router)
 
 	router.Use(func(context *gin.Context) {
@@ -34,6 +59,8 @@ func main() {
 		context.Writer.Header().Add("Access-Control-Allow-Headers", "Content-Type")
 		context.Next()
 	})
-
+	log.Printf("######################################\n")
+	log.Println("#📡 Server starting on localhost:8080#")
+	log.Printf("######################################")
 	log.Fatal(router.Run("localhost:8080"))
 }
